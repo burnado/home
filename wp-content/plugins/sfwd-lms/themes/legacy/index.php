87cca532afac3d2eb2123b9f8e9b6949006b5214
Burnado <burnado@gmx.net>
2020-02-21 20:36:51 +0100

    update

--- wp-content/plugins/sfwd-lms/themes/legacy/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once( 'includes/class-ld-themes-register.php' );
--- wp-content/plugins/sfwd-lms/themes/themes-loader.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once( 'class-ld-themes-register.php' );
-
-// Register ur themes.
-require_once( 'legacy/index.php' );
-require_once( 'ld30/index.php' );
