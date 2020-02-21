580d4f534e381979c2ed8f97c33daf6d1203a6c7
Burnado <burnado@gmx.net>
2020-02-21 20:35:54 +0100

    update

--- wp-content/plugins/learndash-notifications/includes/deactivation.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function learndash_notifications_deactivate() {
-	wp_clear_scheduled_hook( 'learndash_notifications_cron' );
-	wp_clear_scheduled_hook( 'learndash_notifications_cron_hourly' );
-}
-
-register_deactivation_hook( LEARNDASH_NOTIFICATIONS_FILE, 'learndash_notifications_deactivate' );
--- wp-content/plugins/sfwd-lms/themes/ld30/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once( 'includes/class-ld-themes-register.php' );
-require_once( 'includes/class-ld-settings-section-theme-ld30.php' );
